a7b88b3f6d5d0f1161993eb2d9501b8070aa8cae
David Bremner <dbremner@gmail.com>
2015-10-16 21:55:29 -0700

    define WIN32_LEAN_AND_MEAN and add required headers

--- stdafx.h
+++ stdafx.h
@@ -1,8 +1,11 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <windowsx.h>
 #include <richedit.h>
 #include <stdlib.h>
+#include <shellapi.h>
+#include <commdlg.h>
 #include <shlwapi.h>
 #include <strsafe.h>
 
