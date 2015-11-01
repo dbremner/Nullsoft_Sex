a2bba0527c1fc83fc8555b0e04487e10a3e2ac28
David Bremner <dbremner@gmail.com>
2015-10-31 18:59:53 -0700

    add NOMINMAX and silence warnings in WTL and ATL headers

--- stdafx.h
+++ stdafx.h
@@ -1,10 +1,14 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #pragma warning (disable:4820) //padded structs
 #pragma warning (disable:4710) //fn not inlined
 #pragma warning (disable:4365) //type cast, signed/unsigned mismatch
 #pragma warning (disable:4191) //unsafe conversion from FARPROC
 #pragma warning (disable:4265) //class has virtual fns but dtor is not virtual
+#pragma warning (disable:4625) //copy ctor could not be generated
+#pragma warning (disable:4626) //assignment operator could not be generated
+#pragma warning (disable:4302) //truncation from A to B
 #include <windows.h>
 #include <windowsx.h>
 #include <richedit.h>
@@ -22,3 +26,6 @@
 #pragma warning (default:4365)
 #pragma warning (default:4191)
 #pragma warning (default:4265)
+#pragma warning (default:4625)
+#pragma warning (default:4626)
+#pragma warning (default:4302)
